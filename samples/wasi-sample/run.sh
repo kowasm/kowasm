039b8eca6d1efddf3132760bd4b334d3ac21167e
Sébastien Deleuze <sdeleuze@vmware.com>
2023-01-27 12:55:26 +0100

    Polish script output

--- samples/wasi-sample/run.sh
+++ samples/wasi-sample/run.sh
@@ -2,8 +2,10 @@
 
 set -e
 cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-set -ex
+export NODE_NO_WARNINGS=1
 
 ../../gradlew clean compileProductionExecutableKotlinWasm
+echo ""
+echo "Running wasi-sample"
+echo ""
 node --experimental-wasi-unstable-preview1 --experimental-wasm-gc runner.mjs
--- wasi/test.sh
+++ wasi/test.sh
@@ -2,6 +2,7 @@
 
 set -e
 cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+export NODE_NO_WARNINGS=1
 
 ../gradlew clean compileTestProductionExecutableKotlinWasm
 node --experimental-wasi-unstable-preview1 --experimental-wasm-gc $NVM_BIN/mocha -- runner.mjs
