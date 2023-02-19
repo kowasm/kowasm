173ea8cbedccab46ac63888acb59f04a11aa78b9
Sébastien Deleuze <sdeleuze@vmware.com>
2023-02-19 13:10:39 +0100

    Polish sample

--- samples/wasi-sample/src/wasmMain/kotlin/WasiSample.kt
+++ samples/wasi-sample/src/wasmMain/kotlin/WasiSample.kt
@@ -18,17 +18,45 @@ import kotlinx.datetime.Clock
 import org.kowasm.wasi.*
 
 fun main() {
+    print()
+    clock()
+    random()
+    filesystem()
+}
+
+private fun print() {
+    Wasi.out.println("== Print ==")
+
     Wasi.out.println("Hello, world!")
     Wasi.err.println("Error")
+}
 
-    Wasi.out.println(Clock.System.now().toString())
+private fun clock() {
+    Wasi.out.println("\n== Clock ==")
+
+    val now = Clock.System.now()
+    Wasi.out.println(now)
+}
+
+private fun filesystem() {
+    Wasi.out.println("\n== Filesystem ==")
 
     Wasi.createDirectoryAt(StandardDescriptor.FIRST_PREOPEN,"testDir")
-    val descriptor = Wasi.openAt(StandardDescriptor.FIRST_PREOPEN,"testFile", OpenFlags(create = true), DescriptorFlags(read = true, write = true))
+    val descriptor = Wasi.openAt(
+        StandardDescriptor.FIRST_PREOPEN,
+        "testFile",
+        OpenFlags(create = true),
+        DescriptorFlags(read = true, write = true)
+    )
     val content = "Hello, file!"
     Wasi.write(descriptor, content.encodeToByteArray())
-    Wasi.out.println(Wasi.read(descriptor, content.length.toULong()).first.decodeToString())
+    val readResult = Wasi.read(descriptor, content.length.toULong())
+    Wasi.out.println(readResult.first.decodeToString())
     Wasi.readDirectory(StandardDescriptor.FIRST_PREOPEN,".").forEach { Wasi.out.println(it) }
+}
+
+private fun random() {
+    Wasi.out.println("\n== Random ==")
 
     val pseudoGenerator = SeededWasiRandom()
     Wasi.out.println("Pseudo random number generator")
