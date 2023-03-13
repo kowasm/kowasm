8666a9700463921a4dcedb5b98bf5ccbde385a3c
Sébastien Deleuze <sdeleuze@vmware.com>
2023-03-13 14:15:31 +0100

    Cleanup

--- samples/fullstack-sample/client/src/wasmMain/kotlin/UserComponent.kt
+++ samples/fullstack-sample/client/src/wasmMain/kotlin/UserComponent.kt
@@ -1,9 +1,4 @@
 import kotlinx.browser.window
-import kotlinx.html.dom.append
-import kotlinx.html.h3
-import kotlinx.html.input
-import kotlinx.html.js.onKeyUpFunction
-import kotlinx.html.p
 import org.w3c.dom.Element
 import org.w3c.dom.HTMLInputElement
 import reactivity.computed
