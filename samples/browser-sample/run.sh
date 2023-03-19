4ac9e478c57a1bdf431359702a4bfe9bdfc727c3
Sébastien Deleuze <sdeleuze@vmware.com>
2023-03-19 09:58:16 +0100

    Remove browser-sample

--- samples/browser-sample/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-../../gradlew wasmBrowserDistribution
-pwd
-cp build/compileSync/wasm/main/productionExecutable/kotlin/kowasm-browser-sample-wasm.wasm build/distributions
-python3 -m http.server -d build/distributions
